baa2be5ab1405ea6408c763166bc64d4165b7ebd
Naresh Kollipora <53150348+naresh9949@users.noreply.github.com>
2020-08-22 18:16:29 +0530

    Update Lazy_SegmentTree.cpp

--- Lazy_SegmentTree.cpp
+++ Lazy_SegmentTree.cpp
@@ -1,225 +1,175 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 class SegmentTree
 {
-	int array_size;
-	int seg_tree_size;
-	int *input,*Segment_Tree,*lazy;
-public:
-	SegmentTree(int a[],int n)
-	{
-		array_size = n;
+    public:
+    int array_size;
+    int seg_tree_size;
+    int *input,*Segment_Tree,*lazy;
+    
+    public:
+    SegmentTree(int a[],int n)
+    {
+        array_size = n;
+        input = new int[n];
+        
+        for(int i=0;i<n;i++)
+            input[i] = a[i];
 
-		input = new int[n];
+        if(isPowerOfTwo(n))
+            seg_tree_size = (2*n)-1;
+        else
+        {
+            int k = nextPowerOf2(n);
+            seg_tree_size = (2*k)-1;
+        }
 
-		for(int i=0;i<n;i++)
-			input[i] = a[i];
+        Segment_Tree = new int[seg_tree_size];
 
-		if(isPowerOfTwo(n))
-			seg_tree_size = (2*n)-1;
-		else
-		{
-			int k = nextPowerOf2(n);
-			seg_tree_size = (2*k)-1;
-		}
-
-		Segment_Tree = new int[seg_tree_size];
-
-		lazy = new int[seg_tree_size];
-
-
-		for (int i = 0; i < seg_tree_size; i++){
-			Segment_Tree[i] = INT_MAX;
-			lazy[i] = 0;
-		}
-
-		Construtc_Segment_Tree(0,n-1,0);
-
-	}
+        lazy = new int[seg_tree_size];
 
 
+        for (int i = 0; i < seg_tree_size; i++){
+            Segment_Tree[i] = INT_MIN;
+            lazy[i] = 0;
+        }
 
+        Construtc_Segment_Tree(0,n-1,0);
+    }
+    
+    
+    
+    void Construtc_Segment_Tree(int low,int high,int pos)
+    {
+        if(low == high)
+        {
+            Segment_Tree[pos] = input[low];
+            return;
+        }
+        
+        int mid = (low+high)/2;
+        Construtc_Segment_Tree(low,mid,(2*pos)+1);
+        Construtc_Segment_Tree(mid+1,high,(2*pos)+2);
+        
+        Segment_Tree[pos] = max(Segment_Tree[(2*pos)+1],Segment_Tree[(2*pos)+2]);
+    }
+    
+    
+    
+    void UpdateRange(int startRange, int endRange,int delta, int low, int high, int pos)
+    {
+        if(low>high)
+        return;
+        
+        if(lazy[pos] != 0)
+        {
+            Segment_Tree[pos] += lazy[pos];
+            
+            if(low!=high)
+            {
+                lazy[(2*pos)+1] = lazy[pos];
+                lazy[(2*pos)+2] = lazy[pos];
+            }
+            lazy[pos] = 0;
+        }
+        
+        
+        //no overlap condition
+        if(startRange > high || endRange < low) {
+            return;
+        }
+        
+         //total overlap condition
+        if(startRange <= low && endRange >= high) {
+            Segment_Tree[pos] += delta;
+            
+            if(low != high) {
+                lazy[2*pos + 1] += delta;
+                lazy[2*pos + 2] += delta;
+            }
+            return;
+        }
+        
+        int mid = (low+high)/2;
+        
+        UpdateRange(startRange,endRange,delta,low,mid,(2*pos)+1);
+        UpdateRange(startRange,endRange,delta,mid+1,high,(2*pos)+2);
+        
+        Segment_Tree[pos] = max(Segment_Tree[2*pos + 1], Segment_Tree[2*pos + 2]);
+        
 
-	void show()
-	{
+    }
+    
+    
+    
+    int RangeQuery(int qlow, int qhigh,int low, int high, int pos)
+    {
+        if(low > high) {
+            return INT_MIN;
+        }
 
-		cout<<"SEG ";
-		for (int i = 0; i < seg_tree_size; i++)
-			cout<<Segment_Tree[i]<<" ";
-		cout<<endl;
+        //make sure all propagation is done at pos. If not update tree
+        //at pos and mark its children for lazy propagation.
+        if (lazy[pos] != 0) {
+            Segment_Tree[pos] += lazy[pos];
+            
+            if (low != high) { //not a leaf node
+                lazy[2 * pos + 1] += lazy[pos];
+                lazy[2 * pos + 2] += lazy[pos];
+            }
+            lazy[pos] = 0;
+        }
 
-		cout<<"lazy ";
-		for (int i = 0; i < seg_tree_size; i++)
-			cout<<lazy[i]<<" ";
-		cout<<endl;
-	}
+        //no overlap
+        if(qlow > high || qhigh < low){
+            return INT_MIN;
+        }
 
+        //total overlap
+        if(qlow <= low && qhigh >= high){
+            return Segment_Tree[pos];
+        }
 
-
-	void Construtc_Segment_Tree(int low,int high,int pos)
-	{
-		if(low==high)
-		{
-			Segment_Tree[pos] = input[low];
-			return;
-		}
-
-		int mid = (low+high)/2;
-
-		Construtc_Segment_Tree(low,mid,(2*pos)+1);
-		Construtc_Segment_Tree(mid+1,high,(2*pos)+2);
-
-		Segment_Tree[pos] = min(Segment_Tree[(2*pos)+1],Segment_Tree[(2*pos)+2]);
-	}
-
-
-
-
-	void Update_Range(int l,int r,int new_value)
-	{
-		Update_Utility(0,array_size-1,l,r,0,new_value);
-
-	}
-
-	void Update_Index(int pos,int value)
-	{
-		Update_Utility(0,array_size-1,pos,pos,0,value);
-	}
-
-
-
-
-
-	void Update_Utility(int low,int high,int l,int r,int pos,int new_value)
-	{
-		if(low>high)
-			return;
-
-		if(lazy[pos]!=0)
-		{
-			Segment_Tree[pos] += lazy[pos]; // update seg tree with lazy value
-			
-			if(low!=high){
-			lazy[(2*pos)+1] = lazy[pos];    // push lazy value to the child if it is not a leaf
-			lazy[(2*pos)+2] = lazy[pos];
-			}
-
-			lazy[pos] = 0;  //put lazy[pos] = 0 to delete lazy value
-		}
-
-		if(l>high || r<low)
-			return;
-
-		if(low>=l && high<=r){
-			
-			Segment_Tree[pos] += new_value; // update seg tree with lazy value
-			
-			if(low!=high){
-			lazy[(2*pos)+1] = new_value;    // push lazy value to the child if it is not a leaf
-			lazy[(2*pos)+2] = new_value;
-			}
-
-			return;
-		}
-
-		int mid = (low+high)/2;
-
-		Update_Utility(low,mid,l,r,(2*pos)+1,new_value);
-
-		Update_Utility(mid+1,high,l,r,(2*pos)+2,new_value);
-
-		Segment_Tree[pos] = min(Segment_Tree[(2*pos)+1],Segment_Tree[(2*pos)+2]);
-
-	}
-
-
-
-
-	int Range_Query(int l,int r)
-	{
-		return Range_Query_Utility(0,array_size-1,l,r,0);
-	}
-
-
-
-	int Range_Query_Utility(int low,int high,int l,int r,int pos)
-	{
-		if(low>high)
-			return INT_MAX;
-		if(lazy[pos]!=0)
-		{
-			Segment_Tree[pos] += lazy[pos];
-
-			if(low!=high)
-			{
-				lazy[(2*pos)+1] = lazy[pos];
-				lazy[(2*pos)+2] = lazy[pos];
-			}
-
-			lazy[pos] = 0;
-		}
-
-		if(l>high || r<low)
-			return INT_MAX;
-
-		if(low>=l && high<=r)
-			return Segment_Tree[pos];
-
-		int mid = (low+high)/2;
-
-		return min(Range_Query_Utility(low,mid,l,r,(2*pos)+1),
-			Range_Query_Utility(mid+1,high,l,r,(2*pos)+2));
-
-	}
-
-
-
-
-	bool isPowerOfTwo (int x)  
-	{  
+        //partial overlap
+        int mid = (low+high)/2;
+        return max(RangeQuery(qlow, qhigh,
+                        low, mid, 2 * pos + 1),
+                RangeQuery(qlow, qhigh,
+                        mid + 1, high, 2 * pos + 2));
+    }
+    
+    
+    bool isPowerOfTwo (int x)  
+    {  
     /* First x in the below expression is for the case when x is 0 */
     return x && (!(x&(x-1)));  
-	}
+    }
 
 
-	int nextPowerOf2(int n)  
-	{  
-    	int p = 1;  
-    	if (n && !(n & (n - 1)))  
+    int nextPowerOf2(int n)  
+    {  
+        int p = 1;  
+        if (n && !(n & (n - 1)))  
         return n;  
   
-    	while (p < n)  
-        	p <<= 1;  
+        while (p < n)  
+            p <<= 1;  
         return p;
       
-	}  
-
-	//~SegmentTree();
-	
+    }  
+    
+    
 };
 
 int main()
 {
-	int n=5;
-	int a[] = {1,2,4,-2,6};
-
-	SegmentTree obj(a,n);
-
-	//obj.show();
-
-	obj.Update_Range(1,4,100);
-
-	obj.Update_Range(0,2,100);
-
-	obj.Update_Range(3,4,100);
-
-	obj.Update_Index(2,500);
-
-	//obj.show();
-
-	cout<<obj.Range_Query(2,2)<<endl;
-
-
-
+    int a[] = {0,0,0,0,0};
+    
+    SegmentTree obj(a,5);
+    
+    obj.UpdateRange(0,2,100,0,4,0);
+    
+    std::cout << obj.RangeQuery(0,2,0,4,0) << std::endl;
+    
 }
